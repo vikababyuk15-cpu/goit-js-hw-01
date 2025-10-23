c7600a69f9404ae6cb08b3885fa61fb7f3dd31a3
Vika <vikababyuk15@gmail.com>
2025-10-23 10:36:07 +0300

    updated

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
-return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+const totalPrice = quantity * pricePerDroid;
+return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 };
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
